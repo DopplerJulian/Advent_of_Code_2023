52dd9d0b29fcdfa31a665de6428f1336c237a733
Julian <julian@yanik.at>
2023-12-15 13:12:04 +0100

    Add benchmarks

---
+++ benches/bigboy_benchmark.rs
@@ -0,0 +1,28 @@
+use advent_of_code_2023::days::*;
+use criterion::{
+    black_box,
+    criterion_group,
+    criterion_main,
+    Criterion
+};
+use advent_of_code_2023::test_utils;
+
+fn day_15_silver_benchmark(c: &mut Criterion) {
+    let bigboy = test_utils::read_file_as_string(r"./big_bois/berb_d15.txt").unwrap();
+
+    c.bench_function(
+        "calculate silver",
+        |b| b.iter(|| day_15::part_1(&bigboy))
+    );
+}
+fn day_15_gold_benchmark(c: &mut Criterion) {
+    let bigboy = test_utils::read_file_as_string(r"./big_bois/bb_d15.txt").unwrap();
+
+    c.bench_function(
+        "calculate gold",
+        |b| b.iter(|| day_15::part_2(&bigboy))
+    );
+}
+
+criterion_group!(day_15, day_15_silver_benchmark,day_15_gold_benchmark);
+criterion_main!(day_15);
