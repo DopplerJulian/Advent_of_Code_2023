e65a199ce67e0437fcf37ba9ee8c5734b5273caa
Julian <julian@yanik.at>
2023-12-17 21:09:57 +0100

    Add Benchmarks for day 17

--- benches/bigboy_benchmark.rs
+++ benches/bigboy_benchmark.rs
@@ -39,8 +39,25 @@ fn day_16_gold_benchmark(c: &mut Criterion) {
         |b| b.iter(|| day_16::part_2(&bigboy))
     );
 }
+fn day_17_silver_benchmark(c: &mut Criterion) {
+    let bigboy = test_utils::read_file_as_string(r"./test_data/day_17_p.txt").unwrap();
+
+    c.bench_function(
+        "day 17 calculate silver",
+        |b| b.iter(|| day_17::part_1(&bigboy))
+    );
+}
+fn day_17_gold_benchmark(c: &mut Criterion) {
+    let bigboy = test_utils::read_file_as_string(r"./test_data/day_17_p.txt").unwrap();
+
+    c.bench_function(
+        "day 17 calculate gold",
+        |b| b.iter(|| day_16::part_2(&bigboy))
+    );
+}
 
 
 criterion_group!(day_15, day_15_silver_benchmark, day_15_gold_benchmark);
 criterion_group!(day_16, day_16_silver_benchmark, day_16_gold_benchmark);
-criterion_main!(day_16);
+criterion_group!(day_17, day_17_silver_benchmark, day_17_gold_benchmark);
+criterion_main!(day_17);
