65e274190ea53d5eeae5de5372a24fbee8690ca7
Julian <julian@yanik.at>
2023-12-17 14:37:01 +0100

    Add day 16 benchmark

--- benches/bigboy_benchmark.rs
+++ benches/bigboy_benchmark.rs
@@ -1,6 +1,6 @@
 use advent_of_code_2023::days::*;
 use criterion::{
-    black_box,
+    // black_box,
     criterion_group,
     criterion_main,
     Criterion
@@ -11,7 +11,7 @@ fn day_15_silver_benchmark(c: &mut Criterion) {
     let bigboy = test_utils::read_file_as_string(r"./big_bois/berb_d15.txt").unwrap();
 
     c.bench_function(
-        "calculate silver",
+        "day 15 calculate silver",
         |b| b.iter(|| day_15::part_1(&bigboy))
     );
 }
@@ -19,10 +19,28 @@ fn day_15_gold_benchmark(c: &mut Criterion) {
     let bigboy = test_utils::read_file_as_string(r"./big_bois/berb_d15.txt").unwrap();
 
     c.bench_function(
-        "calculate gold",
+        "day 15 calculate gold",
         |b| b.iter(|| day_15::part_2(&bigboy))
     );
 }
+fn day_16_silver_benchmark(c: &mut Criterion) {
+    let bigboy = test_utils::read_file_as_string(r"./test_data/day_16_p.txt").unwrap();
 
-criterion_group!(day_15, day_15_silver_benchmark,day_15_gold_benchmark);
-criterion_main!(day_15);
+    c.bench_function(
+        "day 16 calculate silver",
+        |b| b.iter(|| day_16::part_1(&bigboy))
+    );
+}
+fn day_16_gold_benchmark(c: &mut Criterion) {
+    let bigboy = test_utils::read_file_as_string(r"./test_data/day_16_p.txt").unwrap();
+
+    c.bench_function(
+        "day 16 calculate gold",
+        |b| b.iter(|| day_16::part_2(&bigboy))
+    );
+}
+
+
+criterion_group!(day_15, day_15_silver_benchmark, day_15_gold_benchmark);
+criterion_group!(day_16, day_16_silver_benchmark, day_16_gold_benchmark);
+criterion_main!(day_16);
